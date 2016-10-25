5b73cce5ebcda6f5cde9654d66aa53f671cef5a8
Eugeniu <rusu.eugeniu@gmx.de>
2016-10-25 08:20:49 +0200

    Small improvements / error handling.

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,10 @@ var request = require('request');
 var fs = require('fs');
 var program = require('commander'); 
 
+// Makes sure we don't throw error when handling 
+// self signed certificates.
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 program
   .version('0.0.1')
   .option('-c --cert <path>', 'The client certificate.')
@@ -9,19 +13,25 @@ program
   .option('-t --target <uri>', 'Url of the Solid server where the data will be stored.')
   .option('-s --source <uri>', 'The endpoint from where the data will be taken.')
   .option('-i --interval <sec>', 'The frequency of data pull / uploads.')
+  .option('-d --delegate', 'Use webid delegation.')
   .parse(process.argv);
 
 if (!(program.cert && program.key && program.source && program.target)) {
-  console.log('Invalid command line arguments.')
+  console.log('\n Invalid command line arguments. \n')
+  program.outputHelp()
   return
 }
 
-const certFile = fs.readFileSync(program.cert, 'utf8')
-const keyFile = fs.readFileSync(program.key, 'utf8')
+try {
+  const certFile = fs.readFileSync(program.cert, 'utf8')
+  const keyFile = fs.readFileSync(program.key, 'utf8')
+} catch (e) {
+  console.log('Error occured while reading certificate / key files.')
+  return
+}
+
 const targetUrl = program.target
 const sourceUrl = program.source
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const options = {
   url: program.source,
@@ -29,24 +39,28 @@ const options = {
   key: keyFile
 }
 
+
 const fetchData = () => {
-  console.log('Fetching new data.')
+  console.log(`Fetching new data from ${options.url}.`)
   request.get(options, (err, res) => {
     if (err) {
-      console.log(err)
+      console.log(`Error occured while fetching the data. ${err}`)
       return
     }
-    const putOptions = Object.assign({}, options, {
+
+    const putOptions = {
       url: program.target,
+      cert: certFile,
+      key: keyFile,
       body: res.body
-    }) 
+    } 
 
     request.put(putOptions, (err,res) => {
       if (err) {
-        console.log(err)
+        console.log(`Error occured while uploading the data. ${err}`)
         return
       }
-      console.log('New data uploaded.')
+      console.log(`New data uploaded to ${putOptions.url}.`)
       setTimeout(fetchData, 10000)
     })
   })
